13f49ce9a0d45e9a81d29691581ac661c1ba5dc5
AdvanceQxj <advanceqxj@AdvanceQxjdeMacBook-Pro.local>
2020-09-13 16:05:38 +0800

    用队列实现栈

---
+++ 栈/用队列实现栈/solution.cpp
@@ -0,0 +1,55 @@
+class MyStack {
+public:
+    queue<int> que1;
+    queue<int> que2;
+    /** Initialize your data structure here. */
+    MyStack() {
+
+    }
+
+    /** Push element x onto stack. */
+    void push(int x) {
+        que1.push(x);
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    int pop() {
+        int size = que1.size();
+        while (--size){
+            que2.push(que1.front());
+            que1.pop();
+        }
+        int res = que1.front();
+        que1.pop();
+        que1 = que2;
+        while (!que2.empty()){
+            que2.pop();
+        }
+        return res;
+    }
+    
+    /** Get the top element. */
+    int top() {
+        int res = que1.back();
+        return res;
+    }
+    
+    /** Returns whether the stack is empty. */
+    bool empty() {
+        if (que1.empty()){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
