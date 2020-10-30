a4f3352203fa701fb837b0a623193c953946c4b3
AdvanceQxj <advanceqxj@AdvanceQxjdeMacBook-Pro.local>
2020-10-30 15:34:43 +0800

    239. 滑动窗口最大值

--- 字符串/实现 strStr()/solution.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:
-        if (len(needle)==0):
-            return 0
-        else:
-            if (needle in haystack):
-                return haystack.index(needle)
-            else:
-                return -1
